4fdd9de328442f964badfa8fd1b3e51e1161f767
Jayesh Soni <sonijayesh12345@gmail.com>
2023-05-22 22:57:03 +0530

    unnecessary files removed

--- index.js
+++ index.js
@@ -25,6 +25,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 /* ROUTES */
+app.get("/", (req, res) => {
+  res.send('App is running...!!');
+});
 app.use("/kpi", kpiRoutes);
 app.use("/product", productRoutes);
 app.use("/transaction", transactionRoutes);
